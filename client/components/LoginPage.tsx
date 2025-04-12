ebaadf081445408fe7020ba642023e4d5d5e3ff9
preschmf <preschmf@gmail.com>
2025-04-12 10:27:18 -0400

    little mixup

--- client/components/LoginPage.tsx
+++ client/components/LoginPage.tsx
@@ -1,36 +1,66 @@
-const path = require('path')
-const outputDirectory = path.resolve(__dirname, 'public')
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faGithub, faGoogle } from '@fortawesome/free-brands-svg-icons'
 
-module.exports = {
-  mode: 'development',
-  entry: './index.tsx',
-  output: {
-    path: outputDirectory,
-    filename: 'main.js',
-    publicPath: '/',
-  },
-  target: 'web',
-  devServer: {
-    port: '9000',
-    static: ['./public'],
-    open: true,
-    hot: true,
-    liveReload: true,
-    historyApiFallback: true,
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
-    ],
-  },
+const LogInPage = () => {
+  const handleGitHubLogin = () => {
+    window.location.href = 'http://localhost:8080/api/v1/auth/github'
+  }
+
+  return (
+    <section className="vh-100">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 text-black">
+            <div>
+              <img src="/boards.png" alt="Boards" />
+            </div>
+
+            <div className="d-flex justify-content-center align-items-center">
+              <form className="d-flex flex-column mt-5 w-23">
+                <h1 className="fw-light" style={{ color: 'cadetblue' }}>
+                  Log In
+                </h1>
+                <hr className="mt-2 mb-4"></hr>
+                <button
+                  className="btn btn-lg btn-block btn-primary mb-2"
+                  style={{ backgroundColor: 'black', borderColor: 'black' }}
+                  type="button"
+                  onClick={handleGitHubLogin}
+                >
+                  <FontAwesomeIcon icon={faGithub} className="me-2" /> Sign in with GitHub
+                </button>
+                <button
+                  className="btn btn-lg btn-block btn-primary mb-2"
+                  style={{ backgroundColor: '#dd4b39', borderColor: '#dd4b39' }}
+                  type="button"
+                  disabled
+                >
+                  <FontAwesomeIcon icon={faGoogle} className="me-2" /> Sign in with Google
+                </button>
+                <button
+                  className="btn btn-lg btn-block btn-primary mb-2"
+                  style={{ backgroundColor: '#3b5998', borderColor: '#3b5998' }}
+                  type="button"
+                  disabled
+                >
+                  <FontAwesomeIcon icon={faFacebookF} className="me-2" /> Sign in with Facebook
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="col-sm-6 px-0 d-none d-sm-block">
+            <img
+              src={'/notebook_splash.jpg'}
+              alt="Login image"
+              className="w-100 vh-100"
+              style={{ objectFit: 'cover', objectPosition: 'left' }}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default LogInPage
