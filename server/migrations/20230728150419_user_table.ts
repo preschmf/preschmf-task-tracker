630c222cde65f373b6fc58e898d945e3152fcdf3
preschmf <preschmf@gmail.com>
2025-04-22 01:59:14 -0400

    enforce userid uniqueness for postgres

--- server/migrations/20230728150419_user_table.ts
+++ server/migrations/20230728150419_user_table.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('user', function (table) {
     table.string('id', 18).primary()
-    table.string('userId', 128).notNullable()
+    table.string('userId', 128).notNullable().unique()
     table.string('username', 128).notNullable()
     table.string('createdAt', 25).notNullable()
   })
