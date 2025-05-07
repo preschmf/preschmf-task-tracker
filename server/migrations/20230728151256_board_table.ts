56e1bdb4862df6c9a5bd35fec9c677fe44812157
preschmf <preschmf@gmail.com>
2025-05-06 21:42:49 -0400

    owned by points to userid not user, need more characters

--- server/migrations/20230728151256_board_table.ts
+++ server/migrations/20230728151256_board_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('board', function (table) {
     table.string('boardId', 18).primary()
     table.string('createdAt', 25).notNullable()
-    table.string('ownedBy', 18).notNullable()
+    table.string('ownedBy', 128).notNullable()
     table.string('title', 64).notNullable()
     table.foreign('ownedBy').references('userId').inTable('user')
   })
